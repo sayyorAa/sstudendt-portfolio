f8dc35382ed5fa407cf24012d556dd0dc33018b8
sayyorAa <buranovasayyora7@gmail.com>
2026-05-16 17:59:11 +0500

    added files

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const footer__text = document.querySelector('.footer__text')
+const current_year = new Date().getFullYear()
+
+footer__text.textContent = `@${current_year} Frontend Developer. All rights reserved.`
